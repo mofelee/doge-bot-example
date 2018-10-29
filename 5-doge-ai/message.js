eab0cb34580ca16e3bd0d73ed5455bbb9bd7d507
Mofe Lee <mofe@me.com>
2018-10-30 01:25:28 +0800

    update ai bot

--- 5-doge-ai/message.js
+++ 5-doge-ai/message.js
@@ -3,10 +3,11 @@ module.exports = async function(msg, {tulingBot}) {
     return;
   }
 
-  if (/狗蛋/.test(msg.text())) {
-    tulingBot.setKey('TULING_KEY_IN_HERE');
+  if (/@狗蛋/.test(msg.text())) {
+    tulingBot.setKey('TULING123-API-key');
+    const text = msg.text().replace('@狗蛋')
 
-    const res = await tulingBot.ask(msg.text(), msg.from().id.replace('@', ''));
+    const res = await tulingBot.ask(text, msg.from().id.replace('@', ''));
 
     return msg.say(res, msg.from())
   }
