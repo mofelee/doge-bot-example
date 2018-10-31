8bf36e8fe4e09fdc6051aeca4ec45c4c8d6393d7
Mofe Lee <mofe@me.com>
2018-10-31 11:43:02 +0800

    remove unused code

--- 6-simsimi/message.js
+++ 6-simsimi/message.js
@@ -1,22 +1,18 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
 
-module.exports = async function(msg, {tulingBot}) {
-  //if (msg.self()) {
-  //return;
-  //}
-
+module.exports = async function(msg) {
   if (/@狗蛋/.test(msg.text())) {
-    const text = msg.text().replace('@狗蛋');
-    const key = '51fa5398-286d-4fd6-b441-8b043319f242';
-    const language = 'zh';
-    const ft = '0.0';
+    const text = msg.text().replace('@狗蛋')
+    const key = 'API_KEY' // api key
+    const language = 'zh' // 语言
+    const ft = '1.0' // 0.0 - 1.0 脏话过滤
 
     const res = await fetch(
       `http://sandbox.api.simsimi.com/request.p?key=${key}&lc=${language}&ft=${ft}&text=${encodeURIComponent(
         text,
       )}`,
-    ).then(res=>res.json());
+    ).then(res=>res.json())
 
     return msg.say(res.response, msg.from())
   }
-};
+}
